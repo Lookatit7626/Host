b892bc23fb13f3ee1cbe39b8449f9f3ffd49a03f
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-03-03 17:38:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var connectionString = process.env.TestProGresPost1Link + "?sslmode=require";
 
 const client = new Client({
     user: process.env.TestProGresPost1UserName + "?sslmode=require",
-    host: process.env.TestProGresPost1Link + "?sslmode=require",
+    host: process.env.TestProGresPost1HostName + "?sslmode=require",
     database: process.env.TestProGresPost1Database + "?sslmode=require",
     password: process.env.TestProGresPost1Password + "?sslmode=require",
     port: process.env.TestProGresPost1Port, // Default PostgreSQL port
