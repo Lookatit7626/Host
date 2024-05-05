c63101353851cc73f17a753b8030254ef3bbeb70
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-05-04 20:26:04 -0400

    Create dmpeople.js

---
+++ commands/dmpeople.js
@@ -0,0 +1,54 @@
+console.log('Setup-Server.js');
+const { GatewayIntentBits,SlashCommandBuilder, ChannelType, PermissionsBitField, Client } = require('discord.js');
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.DirectMessages,
+    ]
+})
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('setup-server')
+		.setDescription('Set-up server, only server owner can use this.'),
+  .addStringOption(option =>
+      option
+        .setName('user')
+        .setDescription('give me userid')
+        .setRequired(true) // You can set this to false if the field is optional
+    ),
+  .addStringOption(option =>
+      option
+        .setName('message')
+        .setDescription('message')
+        .setRequired(true) // You can set this to false if the field is optional
+    ),
+	async execute(interaction) {
+  const userId = interaction.options.getString('user');
+  const message = interaction.options.getString('message');
+
+    if(!interaction.guild) {
+      await interaction.reply({content:'Null', ephemeral: true });
+      return "Avoid"
+    };
+    
+    var User = interaction.member
+    if (!User.permissions.has(PermissionsBitField.Flags.Administrator))     {
+      await interaction.reply({content:'Null', ephemeral: true });
+      return "Avoid"
+    }//await interaction.reply({content:'teh tarik', ephemeral: true });
+    const user = await client.users.fetch(userId, false);
+
+    if (user) {
+      user.send(message);
+      await interaction.reply({content:`DM sent to <@${userId}>!`, ephemeral: true });
+    } else {
+      await interaction.reply({content:`User not Found!`, ephemeral: true });
+    }
+	},
+};
