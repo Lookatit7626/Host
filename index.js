54885a2f39a43317dd10f640d0c4437cfeb2bf03
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-11-09 17:24:15 -0500

    switched to express body

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ const findPersonByName = (Name) => {
 };
 
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
