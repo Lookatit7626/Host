6fb9bd189de0e9920688a40e4ce6211826e72536
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-11-09 09:46:56 -0500

    New commit!

--- commands/CommandPlayer.js
+++ commands/CommandPlayer.js
@@ -9,27 +9,27 @@ function wait(ms) {
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('test')
-		.setDescription('do not use me')
+		.setName('HTTPS SERVER')
+		.setDescription('send a https command to discord bot server for administrative purpose')
         .addStringOption(option =>
             option
-              .setName('message')
-              .setDescription('message1')
+              .setName('command')
+              .setDescription('set command')
               .setRequired(true)
           )
           .addStringOption(option =>
             option
-              .setName('message2')
-              .setDescription('message2')
+              .setName('args')
+              .setDescription('set arguments')
               .setRequired(true)
           ),
 	async execute(interaction) {
 
-    const player = interaction.options.getString('message');
-    const command = interaction.options.getString('message2');
+    const player = interaction.options.getString('command');
+    const command = interaction.options.getString('args');
 
     if(!interaction.guild) {
-      await interaction.reply({content:'It must be in a guild!', ephemeral: true });
+      await interaction.reply({content:'It must be in a guild!', ephemeral: false });
       return "Avoid"
     };
     
@@ -40,8 +40,8 @@ module.exports = {
     const botMember = await guild.members.fetch(guild.members.me.id);
     BotRole = botMember.roles.highest;
     
-    if (User.roles.highest.position = BotRole.position && !User.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
-      await interaction.reply({content:'You must be able to manage messages or have a higher role then me!', ephemeral: true });
+    if (User.roles.highest.position = BotRole.position) {
+      await interaction.reply({content:'403 - Unauthorized access to command!', ephemeral: false });
       return "Avoid"
     }
     
@@ -50,9 +50,9 @@ module.exports = {
             Name: player,
             Command: command,
           });
-          await interaction.reply({content:'hmm (1)', ephemeral: true });
+          await interaction.reply({content:'sent command to server', ephemeral: true });
     } catch (error) {
-        await interaction.reply({content:'hmm', ephemeral: true });
+        await interaction.reply({content:'there was an error sending the command!', ephemeral: true });
     }
     //await interaction.reply({content:'teh tarik', ephemeral: true });
 	},
