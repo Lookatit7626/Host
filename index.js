ddc11f1cddac0cc6372bd4110aeed8a7575eb8c7
Lookatit7626 <105156427+Lookatit7626@users.noreply.github.com>
2024-11-09 17:42:07 -0500

    test

--- index.js
+++ index.js
@@ -146,10 +146,6 @@ app.get('/post/AddMessage', async (req, res) => {
 var ListOfPeopleThatUsedToday = new Array();
 
 app.get('/post/EnterMessage', async (req, res) => {
-  error403(res)
-});
-
-app.post('/post/EnterMessage', async (req, res) => {
   const {Name, Executor, CountryCode, Time} = req.body
   try {
     console.log(req.body);
